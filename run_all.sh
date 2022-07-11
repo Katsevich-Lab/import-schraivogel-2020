aab98055ee9580ce8f36a26fb6d9a24d1c32352d
Tim <tbarry2@andrew.cmu.edu>
2022-07-11 13:00:08 -0400

    comment out download; add mem request to run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,12 @@
 # required R packages: R.utils, RCurl, dplyr, readxl, ondisc
 
+#$ -l m_mem_free=16G
+
 hpcc=$(hostname | grep "hpcc" | wc -l)
 if [[ hpcc ]]
 then
   module load R/R-4.1.2
 fi
-Rscript 1_download_data.R
+# Rscript 1_download_data.R
 Rscript 2_convert_to_odm_ground_truth.R
 Rscript 3_convert_to_odm_screen.R
